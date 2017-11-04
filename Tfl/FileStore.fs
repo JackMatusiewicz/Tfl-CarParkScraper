a398efd4ecb4fb82e8b724c596ee462cfa658db4
JackMatusiewicz <jack.matusiewicz@gmail.com>
2017-11-04 20:34:00 +0000

    Updating filestore agent
    Now stores the data in a text file marked with the date. This means
    you'll get one file per day.

--- Tfl/FileStore.fs
+++ Tfl/FileStore.fs
@@ -3,20 +3,25 @@
 module FileStore =
     open Newtonsoft.Json
     open System.IO
+    open System
     
     let private serialise (xs : 'a list) : string list  =
         List.map (fun o -> JsonConvert.SerializeObject(o)) xs
 
     type FileAgentMessage<'t> = | StoreData of 't list
 
-    type FileAgent<'t>(path : string) =
-        let filePath = path
+    type FileAgent<'t>(directory : string) =
+        let pathToStore = directory
         let agent = MailboxProcessor.Start(fun inbox ->
             let rec loop () = async {
                 let! message = inbox.Receive()
                 match message with
                 | StoreData data ->
                     let jsonData = serialise data
+                    let currentDate = DateTime.UtcNow
+                    let filePath = Path.Combine(pathToStore,
+                                    sprintf "carParkData%s.txt"
+                                    <| currentDate.ToString("yyyy-MM-dd"))
                     File.AppendAllLines(filePath, jsonData)
                     return! loop ()
             }
